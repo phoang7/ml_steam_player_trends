104a9bbcc791527b8e7b452c0c3fe7598b56dc83
behzadbouzar <65989833+behzadbouzar@users.noreply.github.com>
2020-06-03 21:41:25 -0700

    updated

--- steam.py
+++ steam.py
@@ -1,36 +1,132 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
-
 import pandas as pd
 
 
-def future_predictor(game_file, testing_size):
+def predictor(game_file):
     '''
-    A linear regression model which will predict how
-    many users a game will have at some point in the
-    future
+    A regression model which will predict how
+    many users a game will have at some point
     '''
     data = pd.read_csv(game_file)
     total = len(data['Month'])
     data['Month Index'] = pd.DataFrame([x for x in range(total, 0, -1)])
-    features = data['Month Index']
-    labels = data['Avg. Players']
+    features = np.array(data['Month Index'].tolist()).reshape(-1, 1)
+    labels = np.array(data['Avg. Players'].tolist())
     features_train, features_test, labels_train, labels_test = \
-        train_test_split(features, labels, test_size=testing_size)
-    #features_train = features_train.reshape(-1, 1)
-    model = LinearRegression()
-    print("month index:", data['Month Index'])
-    print("features", features_train)
-    print("lables", labels_train)
+        train_test_split(features, labels, test_size=.75)
+    model = RandomForestRegressor(n_estimators=500, oob_score=True,
+                                  random_state=100)
     model.fit(features_train, labels_train)
+    prediction = model.predict(features_test)
+    ac = plt.scatter(features_test, labels_test, c='b', marker='o')
+    pred = plt.scatter(features_test, prediction, c='r', marker='x')
+    plt.title("Predicting # Of Concurrent Players")
+    plt.legend((ac, pred), ('Actual Values', 'Predicted Values'))
+    plt.show()
+    print("Random Regressor Tree Score:", r2_score(labels_test, prediction))
 
 
+def dead_or_not(game_file):
+    '''
+    A regression model which will predict at what point
+    in the future will a game be considered "dead" in terms
+    of number of players
+    '''
+    data = pd.read_csv(game_file)
+    total = len(data['Month'])
+    data['Month Index'] = pd.DataFrame([x for x in range(total, 0, -1)])
+    most_player = np.max(data['Avg. Players'])
+    death_level = most_player / 4
+    features = np.array(data['Month Index'].tolist()).reshape(-1, 1)
+    labels = np.array(data['Avg. Players'].tolist())
+    future = np.arange(total+1, total+20, 1).reshape(-1, 1)
+    model = LinearRegression()
+    model.fit(features, labels)
+    prediction_2 = model.predict(future)
+    prediction = list(prediction_2)
+    prediction = [y if y >= 0 else 0 for y in prediction]
+    pst = plt.scatter(features, labels, c='r', marker='x')
+    ftr = plt.scatter(future, prediction, c='g', marker='o')
+    plt.title("Predicting # of Concurrent Players In The Future")
+    plt.legend((pst, ftr), ('Past Data', 'Expected Future Data'))
+    plt.show()
+    future_months = 1
+    for x in prediction:
+        if x < death_level:
+            return 'In {} months, {} playerbase will be considered' \
+                'dead'.format(future_months, game_file[5:-4])
+        else:
+            future_months += 1
+
+    return 'In the next {} months, we predict that this game {} will' \
+        ' still have an active player base'.format(future_months,
+                                                   game_file[5:-4])
+
+
+def average_predictor(game_file):
+    '''
+    We will determine if a game will be popular by
+    doing a comparison between the avg # of players
+    a game has in the first 3  months following its
+    release compared to the avg # of players in the
+    following 3 months.
+    '''
+    data = pd.read_csv(game_file)
+    total = len(data['Month'])
+    data['Month Index'] = pd.DataFrame([x for x in range(total, 0, -1)])
+    first_6 = data[-6:]
+    average_f6 = np.mean(first_6['Avg. Players'])
+    next_6 = data[-12:-6]
+    avg_next_6 = np.mean(next_6['Avg. Players'])
+    if average_f6 < avg_next_6:
+        return "This game, {}, is increasing in popularity"\
+            " by {}%".format(game_file[5:-4], (avg_next_6/average_f6)-1)
+    else:
+        return "This game, {}, appears not to be increasing"\
+            " in popularity".format(game_file[5:-4])
+
+
+def linear_predictor(game_file):
+    data = pd.read_csv(game_file)
+    total = len(data['Month'])
+    data['Month Index'] = pd.DataFrame([x for x in range(total, 0, -1)])
+    actual_fy = data[-12:-7]
+    first_6 = data[-6:]
+    features = np.array(first_6['Month Index'].tolist()).reshape(-1, 1)
+    labels = np.array(first_6['Avg. Players'].tolist())
+    future = np.arange(7, 12, 1).reshape(-1, 1)
+    model = LinearRegression()
+    model.fit(features, labels)
+    prediction = model.predict(future)
+    prediction = list(prediction)
+    prediction = [y if y >= 0 else 0 for y in prediction]
+    pst = plt.scatter(features, labels, c='r', marker='x')
+    expect = plt.scatter(future, prediction, c='g', marker='o')
+    act = plt.scatter(future, actual_fy['Avg. Players'], c='r', marker='^')
+    plt.title("Popularity Predictor")
+    plt.legend((pst, expect, act), ('First 6 Months', 'Expected Next 6',
+                                    'Actual Next 6'))
+    plt.show()
+    average_fy = np.mean(actual_fy['Avg. Players'])
+    average_predicted = np.mean(prediction)
+    if average_fy < average_predicted:
+        return "This game, {}, is increasing in popularity"\
+            " by {}%".format(game_file[5:-4], (average_predicted/average_fy)-1)
+    else:
+        return "This game, {}, appears not to be increasing in"\
+            " popularity".format(game_file[5:-4])
 
 
 def main():
-    future_predictor('data/csgo.csv', .5)
+    predictor('data/csgo.csv')
+    print(dead_or_not('data/csgo.csv'))
+    print(average_predictor('data/csgo.csv'))
+    print(linear_predictor('data/csgo.csv'))
 
 
 if __name__ == '__main__':
